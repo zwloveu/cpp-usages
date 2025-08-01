a03649d248293669e0af272c0bc3500071f75fdd
Simon ZENG <zengweiking@outlook.com>
2025-08-02 00:30:30 +0800

    update

--- cpp-learnings/src/cprimerplus/src/02_02.c
+++ cpp-learnings/src/cprimerplus/src/02_02.c
@@ -16,8 +16,8 @@ int main(void)
 
 void up_and_down(int n)
 {
-    printf("Level %d: n location %p\n", n, &n); // 1
+    printf("Level %d: n location %p\n", n, (void *)&n); // 1
     if (n < 4)
         up_and_down(n + 1);
-    printf("LEVEL %d: n location %p\n", n, &n); // 2
+    printf("LEVEL %d: n location %p\n", n, (void *)&n); // 2
 }
