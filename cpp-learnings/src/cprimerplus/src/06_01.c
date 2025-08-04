a30f042a6053b93d4c9cb133759374fdc1188ce5
Simon ZENG <zengweiking@outlook.com>
2025-08-04 22:36:12 +0800

    update

--- cpp-learnings/src/cprimerplus/src/06_01.c
+++ cpp-learnings/src/cprimerplus/src/06_01.c
@@ -3,6 +3,7 @@
 #define MONTHS 12
 
 int days_sum(const int *const);
+void print_data(const void *const data, const char type);
 
 int main(void)
 {
@@ -19,18 +20,28 @@ int main(void)
 
     printf("The address of days is %p.\n"
            "The value of p_days is %p.\n"
-           "The address of the first elemment of days is %p.\n"
-           "The address of the second elemment of days is %p.\n"
+           "The address of the first element of days is %p.\n"
+           "The address of the second element of days is %p.\n"
            "The step between the elements of days is %zd.\n"
            "days of one year is %d",
-           days,
-           p_days,
-           &days[0],
-           &days[1],
+           (void *)days,
+           (void *)p_days,
+           (void *)&days[0],
+           (void *)&days[1],
            // address1-address2 with same type = elements count
            // Convert to char* to get bytes count
            (char *)&days[1] - (char *)&days[0],
            days_sum(days));
+
+    int num = 10;
+    float pi = 3.14;
+    char ch = 'A';
+
+    print_data(&num, 'i');
+    print_data(&pi, 'f');
+    print_data(&ch, 'c');
+
+    printf("void * occupies %zd bytes.\n", sizeof(void *));
 
     return 0;
 }
@@ -45,4 +56,21 @@ int days_sum(const int *const days)
     }
 
     return total;
+}
+
+void print_data(const void *const data, const char type)
+{
+    switch (type)
+    {
+    case 'i':
+        printf("Integer: %d\n", *(int *)data);
+        break;
+    case 'f':
+        printf("Float: %.2f\n", *(float *)data);
+        break;
+    case 'c':
+        printf("Character: %c\n", *(char *)data);
+        break;
+    default:;
+    }
 }
